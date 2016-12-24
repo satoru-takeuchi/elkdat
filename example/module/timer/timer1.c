29c56f8539d1c01426ef6d6883798d40418f445d
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2016-12-25 00:16:04 +0900

    add examples about timer
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

---
+++ example/module/timer/timer1.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Satoru Takeuchi <satoru.takeuchi@gmail.com>");
+MODULE_DESCRIPTION("timer kernel module: oneshot timer");
+
+struct timer_list mytimer;
+
+#define MYTIMER_TIMEOUT_SECS	10
+
+static void mytimer_fn(unsigned long arg)
+{
+	printk(KERN_ALERT "10 secs passed.\n");
+}
+
+static int timer_module_init(void)
+{
+	init_timer(&mytimer);
+	mytimer.expires = jiffies + MYTIMER_TIMEOUT_SECS*HZ;
+	mytimer.data = 0;
+	mytimer.function = mytimer_fn;
+	add_timer(&mytimer);
+
+	return 0;
+}
+
+static void timer_module_exit(void)
+{
+	del_timer(&mytimer);
+}
+
+module_init(timer_module_init);
+module_exit(timer_module_exit);
---
+++ example/module/timer/timer4.c
@@ -0,0 +1,64 @@
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Satoru Takeuchi <satoru.takeuchi@gmail.com>");
+MODULE_DESCRIPTION("timer kernel module: pass arg");
+
+struct mytimer_data {
+	char *name;
+	int interval;
+	int count;
+	struct timer_list timer;
+};
+
+struct mytimer_data data[2] = {
+	{
+		.name = "foo",
+		.interval = 2,
+	},
+	{
+		.name = "bar",
+		.interval = 3,
+	},
+};
+
+#define MYTIMER_TIMEOUT_SECS	10
+
+static void mytimer_fn(unsigned long arg)
+{
+	struct mytimer_data *data = (struct mytimer_data *)arg;
+
+	printk(KERN_ALERT "%s: %d secs passed.\n",
+	       data->name, data->interval);
+	
+	mod_timer(&data->timer, jiffies + data->interval*HZ);
+}
+
+static int timer_module_init(void)
+{
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		struct mytimer_data *d = &data[i];
+		init_timer(&d->timer);
+		d->timer.function = mytimer_fn;
+		d->timer.expires = jiffies + d->interval*HZ;
+		d->timer.data = (unsigned long)d;
+		add_timer(&d->timer);
+	}
+
+	return 0;
+}
+
+static void timer_module_exit(void)
+{
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		del_timer(&data[i].timer);
+	}
+}
+
+module_init(timer_module_init);
+module_exit(timer_module_exit);
